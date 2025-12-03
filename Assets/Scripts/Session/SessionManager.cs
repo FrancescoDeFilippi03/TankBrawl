512f694f2b58552690a6d8d13eff22435d8ef26c
Francesco De Filippi <318004@studenti.unimore.it>
2025-12-04 00:46:35 +0100

    feat : create and join session

--- Assets/Scripts/Session/SessionManager.cs
+++ Assets/Scripts/Session/SessionManager.cs
@@ -9,7 +9,8 @@ public class SessionManager : MonoBehaviour
 {
     public static SessionManager Instance { get; private set; }
 
-
+    private ISession currentSession;
+    public ISession CurrentSession => currentSession;
 
     //singleton pattern don't drestroy on load 
     // ensures only one instance of SessionManager exists
@@ -27,28 +28,68 @@ public class SessionManager : MonoBehaviour
         }
     }
 
-    async void Start()
+    private void Onable()
     {
+        //subscribe to events
+    }
+    
+    private void OnDisable()
+    {
+        //unsubscribe from events
+    }
 
-	    try
+    async void Start()
+    {   
+    
+        try
 	    {
-	     await UnityServices.InitializeAsync();
-	     await AuthenticationService.Instance.SignInAnonymouslyAsync();
-	     Debug.Log($"Sign in anonymously succeeded! PlayerID: {AuthenticationService.Instance.PlayerId}");
+            await UnityServices.InitializeAsync();
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            Debug.Log($"Sign in anonymously succeeded! PlayerID: {AuthenticationService.Instance.PlayerId}");
 	    }
 	    catch (Exception e)
 	    {
-	     Debug.LogException(e);
+	        Debug.LogException(e);
 	    }
     }
 
-    async Task StartSessionAsHost()
+    public async Task StartSessionAsHost()
     {
         var options = new SessionOptions
         {
-            MaxPlayers = 2
-        }.WithRelayNetwork(); // or WithDistributedAuthorityNetwork() to use Distributed Authority instead of Relay
-        var session = await MultiplayerService.Instance.CreateSessionAsync(options);
-        Debug.Log($"Session {session.Id} created! Join code: {session.Code}");
+            MaxPlayers = 6
+
+        }.WithRelayNetwork();
+
+        try
+        {
+
+            currentSession = await MultiplayerService.Instance.CreateSessionAsync(options);
+            Debug.Log($"Session {currentSession.Id} created! Join code: {currentSession.Code}");
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            return;
+        }
+      
     }
+
+    public async Task JoinSessionAsClient(string joinCode)
+    {
+        try
+        {
+            currentSession = await MultiplayerService.Instance.JoinSessionByCodeAsync(joinCode);
+            Debug.Log($"Joined session {currentSession.Id} with join code: {joinCode}");
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            return;
+        }
+    }
+
+    
+
 }
